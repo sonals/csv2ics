d8e1f46d24f7524232c8477e72bc15509cb5db8b
Sonal Santan <sonal.santan@xilinx.com>
2022-09-01 16:01:08 -0700

    More code for ics file creation

--- src/csv2ics.py
+++ src/csv2ics.py
@@ -15,21 +15,51 @@ def parseCommandLine(args):
     # strip out the argv[0]
     return parser.parse_args(args[1:])
 
+def openICS(zone):
+    fd = open(zone + ".ics", "w")
+    fd.write("BEGIN:VCALENDAR\n")
+    fd.write("PRODID:-//Dharmic//Calendar //EN\n")
+    fd.write("VERSION:2.0\n")
+    fd.write("CALSCALE:GREGORIAN\n")
+    fd.write("METHOD:PUBLISH\n")
+    fd.write("X-WR-CALNAME: 2022 Hindu Calendar For %s\n" % zone)
+    fd.write("X-WR-CALDESC: 2022 Hindu Calendar For %s\n" % zone)
+    return fd
+
+def openAllICS(row):
+    timezones = []
+    for tz in row:
+        timezones.append(openICS(tz))
+    return timezones
+
+def closeAllICS(timezones):
+    for fd in timezones:
+        fd.write("END:VCALENDAR\n")
+        fd.close()
+
+def printAllICS(festivalTab):
+    timezones = []
+    for row in festivalTab:
+        print(row)
+        if (row[0] == "Timezones"):
+            timezones = openAllICS(row[1:])
+    closeAllICS(timezones)
+
 
 def parseCSV(csvName):
     print(csvName)
     csvFile = open(csvName)
     festivalTab = csv.reader(csvFile)
     print(festivalTab)
-    for row in festivalTab:
-        print(row)
-    close(csvFile)
-
+#    for row in festivalTab:
+#        print(row)
+    printAllICS(festivalTab)
+    csvFile.close()
 
 def main(args):
     argtab = parseCommandLine(args)
     print(argtab)
-    print("Processing CSV file %s\n" % argtab.fname[0]);
+    print("Processing CSV file %s" % argtab.fname[0]);
     parseCSV(argtab.fname[0])
     return 0
 
