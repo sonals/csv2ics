422be46bb72f091256b7d3143468dc6f91e6a170
Sonal Santan <sonal.santan@xilinx.com>
2022-09-02 18:15:47 -0700

    Working code!

--- src/csv2ics.py
+++ src/csv2ics.py
@@ -1,59 +1,64 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright (C) 2022 sonal.santan@gmail.com
+
 import argparse
 import sys
 import csv
 import os
+import ics
+import datetime
 
 def parseCommandLine(args):
     parser = argparse.ArgumentParser(description ='Convert from CVS to ICS')
-
-    parser.add_argument(dest ='fname', metavar ='csv', nargs =1)
-    parser.add_argument('-c', '--column', metavar ='column',
-                        required = True, dest ='col',
-                        action ='store', help ='date column')
-    parser.add_argument('-o', '--output', metavar='ics', dest ='outfile',
-                        required = True, action ='store', help ='output ICS file')
+    parser.add_argument(dest ='fname', metavar ='csv', nargs = 1)
     # strip out the argv[0]
     return parser.parse_args(args[1:])
-
-def openICS(zone):
-    fd = open(zone + ".ics", "w")
-    fd.write("BEGIN:VCALENDAR\n")
-    fd.write("PRODID:-//Dharmic//Calendar //EN\n")
-    fd.write("VERSION:2.0\n")
-    fd.write("CALSCALE:GREGORIAN\n")
-    fd.write("METHOD:PUBLISH\n")
-    fd.write("X-WR-CALNAME: 2022 Hindu Calendar For %s\n" % zone)
-    fd.write("X-WR-CALDESC: 2022 Hindu Calendar For %s\n" % zone)
-    return fd
 
 def openAllICS(row):
     timezones = []
     for tz in row:
-        timezones.append(openICS(tz))
+        pair = tuple((tz, ics.Calendar()))
+        timezones.append(pair)
+        print("Constructing calendar for %s" % pair[0]);
     return timezones
 
-def closeAllICS(timezones):
-    for fd in timezones:
-        fd.write("END:VCALENDAR\n")
+def createEvents(timezones, row):
+    if (len(row) != len(timezones) + 1):
+        return
+    for i in range(0, len(timezones)):
+        e = ics.Event(name = row[0])
+        e.begin = datetime.datetime.strptime(row[i + 1], "%b %d, %y")
+        (timezones[i])[1].events.add(e)
+    print("Processed event %s" % row[0])
+
+def writeAllICS(timezones):
+    for pair in timezones:
+        fd = open(pair[0] + ".ics", "w")
+        fd.write(pair[1].serialize())
         fd.close()
 
-def printAllICS(festivalTab):
+def extractAllICS(festivalTab):
     timezones = []
+    begin = False
     for row in festivalTab:
-        print(row)
         if (row[0] == "Timezones"):
             timezones = openAllICS(row[1:])
-    closeAllICS(timezones)
-
+            begin = True
+            continue
+        if (row[0] == "Major Cities"):
+            continue;
+        if (begin):
+            createEvents(timezones, row)
+    writeAllICS(timezones)
 
 def parseCSV(csvName):
     print(csvName)
     csvFile = open(csvName)
     festivalTab = csv.reader(csvFile)
     print(festivalTab)
-#    for row in festivalTab:
-#        print(row)
-    printAllICS(festivalTab)
+    extractAllICS(festivalTab)
     csvFile.close()
 
 def main(args):
